1a4313b4a2f6ee92f60e71c07b7daa81507302ea
Jakub Szczygieł <116550165+Szczygiel29@users.noreply.github.com>
2023-07-18 13:12:29 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/StudnetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudnetRepository extends JpaRepository<Student, Long> {
-}
