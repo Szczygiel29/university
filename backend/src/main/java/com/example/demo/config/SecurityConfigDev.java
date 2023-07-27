fb459c22a7f5abb9fbee3e055e007f89a6c5c142
Jakub <jakub.szczygiel29@gmail.com>
2023-07-27 16:18:44 +0200

    Frontend

--- backend/src/main/java/com/example/demo/config/SecurityConfigDev.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfigDev.java
@@ -2,18 +2,14 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@Profile("dev")
 public class SecurityConfigDev {
 
     @Bean
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StudentPage from "./components/StudentPage";
+
+function App() {
+  return (
+    <div className="App">
+      <StudentPage/>
+    </div>
+  );
+}
+
+export default App;
